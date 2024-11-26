eb04901db49ebcd4eb2f772d3d4fa5fc756b93f9
raultorresn <raultorresn@hotmail.com>
2024-11-25 22:30:57 -0300

    Commit 4

--- Atividades/Customer.java
+++ Atividades/Customer.java
@@ -25,9 +25,8 @@ public class Customer {
      Enumeration rentals = _rentals.elements();
      String result = "Rental Record for " + getName() + "\n";
      while (rentals.hasMoreElements()) {
-        double thisAmount = 0;
         Rental each = (Rental) rentals.nextElement();
-        thisAmount = amountFor(each);
+        double thisAmount = each.getCharge();
 
         // add frequent renter points
         frequentRenterPoints ++;
@@ -47,24 +46,4 @@ public class Customer {
              " frequent renter points";
      return result;
    }
-   private double amountFor(Rental aRental) {
-    double thisAmount = 0;
-
-    switch (aRental.getMovie().getPriceCode()) {
-        case Movie.REGULAR:
-           thisAmount += 2;
-           if (aRental.getDaysRented() > 2)
-              thisAmount += (aRental.getDaysRented() - 2) * 1.5;
-           break;
-        case Movie.NEW_RELEASE:
-           thisAmount += aRental.getDaysRented() * 3;
-           break;
-        case Movie.CHILDRENS:
-           thisAmount += 1.5;
-           if (aRental.getDaysRented() > 3)
-              thisAmount += (aRental.getDaysRented() - 3) * 1.5;
-            break;
-     }
-      return thisAmount;
- }
 }
